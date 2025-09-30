7043ee0154b868972f535bd002bdf7a279d87fd5
salsiy <siyadsalam@gmail.com>
2025-09-30 23:16:08 +0200

    Remove S3 bucket - test Digger destroy functionality
    This will remove the S3 bucket that was previously created.
    The plan should show 5 resources to be destroyed.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 # Terraform configuration for Digger CI/CD testing
-# This will create an S3 bucket with proper security configurations
-# Testing Digger apply functionality via PR comments
+# S3 bucket removed - testing Digger destroy functionality
 terraform {
   required_version = ">= 1.0"
   required_providers {
@@ -15,21 +14,5 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Use the S3 bucket module
-module "s3_bucket" {
-  source = "../modules/s3-bucket"
-}
-
-# Outputs from the module
-output "bucket_name" {
-  value = module.s3_bucket.bucket_name
-}
-
-output "bucket_arn" {
-  value = module.s3_bucket.bucket_arn
-}
-
-output "bucket_domain_name" {
-  value = module.s3_bucket.bucket_domain_name
-}
+# S3 bucket module removed - this will destroy the existing bucket
 
